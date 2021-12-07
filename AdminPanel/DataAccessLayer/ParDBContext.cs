cf106216034af6caa2cd6a19bd6fe92d69b51668
Aleksandr.Chernyshev@hoff.ru <Aleksandr.Chernyshev@hoff.ru>
2021-12-07 16:40:04 +0300

    add pardb

---
+++ AdminPanel/DataAccessLayer/ParDBContext.cs
@@ -0,0 +1,26 @@
+﻿using AdminPanel.Models.Models.Par_Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.DataAccessLayer
+{
+	public class ParDBContext : DbContext
+	{
+		public ParDBContext(DbContextOptions<ParDBContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+			try
+			{
+				Database.Migrate();
+			}
+			catch { }
+		}
+
+		public DbSet<CategoryModel> categories { get; set; }
+		public DbSet<ParameterModel> parameters { get; set; }
+		public DbSet<ParameterValueModel> parameter_values { get; set; }
+	}
+}
